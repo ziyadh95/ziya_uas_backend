25d09637ce219118e6fd429bb54500334d6a539e
ziyadatun nafisah <ziyadah.nafisah@yahoo.com>
2022-08-17 21:38:13 +0700

    first

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import UserRoute from "./routes/UserRoute.js";
-import { connected } from "process";
+//import { connected } from "process";
 
 const app = express();
 mongoose.connect('mongodb://localhost:27017/fullstack_db',{
@@ -17,4 +17,4 @@ app.use(cors());
 app.use(express.json());
 app.use(UserRoute);
 
-app.listen(5000, ()=>console.log('Server up and running...'));
+app.listen(process.env.PORT || 5000, ()=>console.log('Server up and running...'));
